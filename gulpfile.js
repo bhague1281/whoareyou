e7de0ee5af293df2d689c902a8725677e449d038
bhague1281 <bhague1281@gmail.com>
2016-01-12 22:12:26 -0800

    Added css minify, gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('sass', function() {
   return gulp
   .src('./src/scss/**/*.scss')
   .pipe(sass())
+  .pipe(minifyCss())
   .pipe(gulp.dest('./src/css'));
 });
 
-gulp.task('default', function() {
-  gulp.run('sass');
+gulp.task('watch', ['sass'], function() {
+  return gulp
+  .watch('./src/scss/**/*.scss', ['sass']);
 });
+
+gulp.task('default', ['sass']);
