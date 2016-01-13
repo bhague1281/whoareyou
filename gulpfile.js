4377207c4c5ace899d74c5e7993a7b72fb2ce942
bhague1281 <bhague1281@gmail.com>
2016-01-13 07:46:55 -0800

    Gulp for JS and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,44 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var minifyCss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+
+var sassInput = './src/scss/**/*.scss';
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+var jsInput = './src/app/**/*.js';
 
 gulp.task('sass', function() {
   return gulp
-  .src('./src/scss/**/*.scss')
-  .pipe(sass())
+  .src(sassInput)
+  .pipe(sourcemaps.init())
+  .pipe(sass(sassOptions).on('error', sass.logError))
+  .pipe(autoprefixer())
   .pipe(minifyCss())
+  .pipe(sourcemaps.write())
   .pipe(gulp.dest('./src/css'));
 });
 
-gulp.task('watch', ['sass'], function() {
+gulp.task('js', function() {
   return gulp
-  .watch('./src/scss/**/*.scss', ['sass']);
+  .src(jsInput)
+  .pipe(sourcemaps.init())
+  .pipe(uglify())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./src/js'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('watch', ['sass', 'js'], function() {
+  return gulp
+  .watch('./src/scss/**/*.scss', ['sass'])
+  .on('change', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
+});
+
+gulp.task('default', ['sass', 'js']);
